2a8a99b5cb6343aa18a5bdb6cc27ea1bf2eb3371
Jean-Michel Fayard <jmfayard@gmail.com>
2018-12-01 07:08:55 +0100

    Update root-build.gradle.kts

--- gradle/root-build.gradle.kts
+++ gradle/root-build.gradle.kts
@@ -1,5 +1,20 @@
 // build.gradle.kts
+buildscript {
+  repositories {
+    jcenter()
+    google()
+    maven("https://jitpack.io")
+  }
+  dependencies {
+    // Kotlin, Android, ......
+  }
+}
 
+plugins {
+    id("com.gradle.build-scan") version "1.16"
+    id("jmfayard.github.io.gradle-kotlin-dsl-libs") version "0.2.6"
+    id("org.jlleitschuh.gradle.ktlint") version "6.3.0" apply (false)
+}
 repositories {
     mavenCentral()
     jcenter()
